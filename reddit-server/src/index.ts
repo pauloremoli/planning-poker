23f4ef41994105bc34e0b095c1de68a610cf0310
Paulo Remoli <paulo002@gmail.com>
2021-10-20 01:09:32 +0200

    Setup graphql server

--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,20 +1,41 @@
+import "reflect-metadata";
 import { __prod__ } from './constants';
-import {MikroORM} from "@mikro-orm/core"
-import { Post } from './entities/Post';
-import mikroConfig  from './mikro-orm.config';
-
+import { MikroORM } from "@mikro-orm/core"
+import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from './resolvers/post';
 
 require('dotenv').config();
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
-    //await orm.getMigrator().up();
-    const post = orm.em.create(Post, {title: "My first post"});
-    await orm.em.persistAndFlush(post);
+    await orm.getMigrator().up();
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
-    
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [PostResolver],
+            validate: false,
+        }),
+        context: () => ({
+            em: orm.em
+        })
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
+
+    const port = 3000;
+    app.listen(port, () => {
+        console.log(`App listening at http://localhost:${port}`)
+    })
+
 }
 
 
