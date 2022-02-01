078bca4041a37a4ffc866ae8a78b205d0bbe3c3b
pauloremoli <paulo002@gmail.com>
2022-02-01 02:47:59 +0100

    fix cookie name

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,3 +1,3 @@
 export const __prod__ = process.env.NODEV_ENV === "production";
-export const COOKIE_NAME : string = "reddit";
+export const COOKIE_NAME : string = "sonhoencantado";
 export const FORGET_PASSWORD_PREFIX : string = "forgot-password:";
